090c73bd8c9c05a29318dc85fb95fa2eff03a8e5
JohnNils Olson <johnnils@gmail.com>
2020-08-31 11:12:20 -0700

    add failing test for findQuarters
    Co-authored-by: Steven Ly <1stevenly92@gmail.com>

--- __tests__/coin-counter.test.js
+++ __tests__/coin-counter.test.js
@@ -1,9 +1,16 @@
 import { coinCounter } from './../src/coin-counter.js';
+import { findChange } from './../src/coin-counter.js';
+import { findQuarters } from './../src/coin-counter.js';
 
 describe ('coinCounter', () => {
 
   test('should correctly calculate change needed', () => {
     const number = 4.99;
-    expect(coinCounter(number)).toEqual(.01);
+    expect(findChange(number)).toEqual(.01);
+  });
+
+  test('should correctly calculate quarters needed to make change', () => {
+    const number = 3.25;
+    expect(findQuarters(number)).toEqual(3);
   });
 });
--- src/coin-counter.js
+++ src/coin-counter.js
@@ -1,3 +1,23 @@
 export const coinCounter = (number) => {
+  return findChange(number);
+}
+
+export const findChange = (number) => {
   return parseFloat((Math.round(number)-number).toFixed(2));
+}
+
+export const findQuarters = () => {
+
+}
+
+const findDimes = () => {
+
+}
+
+const findNickels = () => {
+
+}
+
+const findPennies = () => {
+
 }
